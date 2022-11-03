6f6f0ffb80ba55592924ec24b5a6ff5733b1a9af
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-03 18:15:39 +0200

    .

--- src/components/variables.js
+++ src/components/variables.js
@@ -1,4 +1,7 @@
 export const language = {
 	shopName: "Kirjakauppa",
-	shopSlogan: "Joku hyvä slogan"
+	shopSlogan: "Joku hyvä slogan",
+	navHome: "Koti",
+	navAbout: "Tietoja",
+	navShop: "Kauppa"
 }
