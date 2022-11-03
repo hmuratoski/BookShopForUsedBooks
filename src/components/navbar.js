f56bf0ff94625aca674667a424227f55f21ee62d
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-03 18:44:51 +0200

    .

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,24 +6,24 @@ export function Navbar() {
 	return (
 		<div>
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container px-4 px-lg-5">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">{language.navHome}</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">{language.navAbout}</a></li>
-						<li class="nav-item"><a class="nav-link" href="#!">{language.navShop}</a></li>
-                    </ul>
-                    <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
-                            <i class="bi-cart-fill me-1"></i>
-                            {language.navCart}
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+				<div class="container px-4 px-lg-5">
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+							<li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">{language.navHome}</a></li>
+							<li class="nav-item"><a class="nav-link" href="#!">{language.navAbout}</a></li>
+							<li class="nav-item"><a class="nav-link" href="#!">{language.navShop}</a></li>
+						</ul>
+						<form class="d-flex">
+							<button class="btn btn-outline-dark" type="submit">
+								<i class="bi-cart-fill me-1"></i>
+								{language.navCart}
+								<span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+							</button>
+						</form>
+					</div>
+				</div>
+			</nav>
 		</div>
 	);
 }
