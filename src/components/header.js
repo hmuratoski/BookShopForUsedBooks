6aec846bb1e74145fc694d60bef30eb9bdd61bb1
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-03 18:47:25 +0200

    .

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,9 @@ import { language } from './variables.js'
 export function Header() {
 	return (
 		<div>
-           
 			<header class="bg-dark py-5">
 				<div class="container px-4 px-lg-5 my-5">
-				<img src={"../images/book.png"} class="img-fluid" alt="Responsive image"></img>
+				<img src={require('../images/book.png')} class="img-fluid" alt="Responsive image"></img>
 					<div class="text-center text-white">
 						<h1 class="display-4 fw-bolder">{language.shopName}</h1>
 						<p class="lead fw-normal text-white-50 mb-0">{language.shopSlogan}</p>
