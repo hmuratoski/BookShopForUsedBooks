5109796fc4094772b8e89746ace7e7c875b3b0ca
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-03 19:37:12 +0200

    .

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -9,16 +9,16 @@ export function Catalog() {
 		<section class="py-5">
 			<div class="container px-4 px-lg-5 mt-5">
 				<div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-				<Product productPrice="50€" productName="Kirja1" />
-					<Product productPrice="20€" productName="Kirja2" />
-					<Product productPrice="20€" productName="Kirja3" />
-					<Product productPrice="20€" productName="Kirja4" />
-					<Product productPrice="20€" productName="Kirja5" />
-					<Product productPrice="50€" productName="Kirja1" />
-					<Product productPrice="20€" productName="Kirja2" />
-					<Product productPrice="20€" productName="Kirja3" />
-					<Product productPrice="20€" productName="Kirja4" />
-					<Product productPrice="20€" productName="Kirja5" />	
+				<Product productPrice="50" productName="Kirja1" />
+					<Product productPrice="50" productName="Kirja2" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja3" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja4" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja5" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja1" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja2" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja3" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja4" productImage="product.png" />
+					<Product productPrice="50" productName="Kirja5" productImage="product.png" />	
 				</div>
 			</div>
         </section >
--- src/components/product.js
+++ src/components/product.js
@@ -10,11 +10,11 @@ export function Product(props) {
 	<div>
 		<div class="col mb-5">
 			<div class="card h-100">
-			<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+			<img class="card-img-top" src={require('../images/product.png')} alt="..." />
 				<div class="card-body p-4">
 					<div class="text-center">
 						<h5 class="fw-bolder">{props.productName}</h5>
-						{props.productPrice}
+						{props.productPrice}€
 					</div>
 				</div>
 				<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
