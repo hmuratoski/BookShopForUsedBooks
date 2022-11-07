c5ffa6d0173370abf8cfd2fd156e3f191e8e36ff
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-07 11:41:59 +0200

    fix fonts

--- src/components/product.js
+++ src/components/product.js
@@ -2,8 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { language } from '../locale/variables.js'
 
-
-
 //yksittäisen tuotteen komponentti, käytettään Catalogissa tuotteiden näyttämiseen
 //voidaan koodata toimimaan myös ostoskorin tuotteiden näyttämiseen
 
