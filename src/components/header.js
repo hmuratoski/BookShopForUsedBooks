fda743081a2bb135f019ef67dc53d4904864ac98
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-07 10:13:13 +0200

    add header.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { language } from './variables.js'
+import '../css/Header.css';
 
 //logot sun muut
 
@@ -10,7 +11,7 @@ export function Header() {
 			<header class="bg-dark py-5">
 				<div class="container px-4 px-lg-5 my-5 row">
 					<div class="col-sm">
-						<img src={require('../images/book.png')} style={{ width: "60%", display: "inline-block"}} class="img-fluid" />
+						<img src={require('../images/book.png')} className="headerLogo" class="img-fluid" />
 					</div>
 					<div class="text-center text-white col-sm" style={{display: "inline-block"}}>
 						<h1 class="display-4 fw-bolder">{language.shopName}</h1>
