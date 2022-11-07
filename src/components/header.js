d3ce21f8d66e02cbd44e92856d73d263a66edf5c
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-07 10:42:24 +0200

    header changes

--- src/components/header.js
+++ src/components/header.js
@@ -8,12 +8,12 @@ import '../css/Header.css';
 export function Header() {
 	return (
 		<div>
-			<header className="bg-dark py-5">
+			<header className="bg-dark py-5 headerWrapper">
 				<div className="container px-4 px-lg-5 my-5 row">
 					<div className="col-sm">
 						<img src={require('../images/book.png')} className="headerLogo"/>
 					</div>
-					<div className="headerWrapper text-center text-white col-sm">
+					<div className="headerTextWrapper text-center text-white col-sm">
 						<h1 className="display-4 fw-bolder">{language.shopName}</h1>
 						<p className="lead fw-normal text-white-50 mb-0">{language.shopSlogan}</p>
 					</div>
