8ffe0655053a8ac6623f9e1aa865f65016519986
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-07 16:34:14 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './components/header';
 import { Content } from './components/content';
 import { Footer } from './components/footer';
 import { Navbar } from './components/navbar';
-import { Team } from './components/team'
+import { language } from './locale/FI.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
 import './fonts/SignikaNegative-Regular.ttf';
 
 function App() {
+	useEffect(() => {
+		document.title = language.shopName;
+	})
+	
+	
 	
 	return (
 		<div>
