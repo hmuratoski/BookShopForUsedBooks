df400c05e906970caaf18b671252b366d9e4feb7
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-07 18:31:01 +0200

    .

--- src/components/product.js
+++ src/components/product.js
@@ -9,24 +9,24 @@ import '../css/Product.css';
 
 export const Product = (props) => {
 	return (
-	<div className="product">
-		<div className="col">
-			<div className="card">
-			<img className="card-img-top" src={props.productImage} alt="..." />
-				<div className="card-body p-4">
-					<div className="text-center productDetails">
-						<h5 className="fw-bolder bookTitle">{props.productName}</h5>
-						<h6 className="bookAuthor">{props.productAuthor}</h6>
+		<div className="product">
+			<div className="col">
+				<div className="card">
+					<img className="card-img-top" src={props.productImage} alt="..." />
+					<div className="card-body p-4">
+						<div className="text-center productDetails">
+							<h5 className="fw-bolder bookTitle">{props.productName}</h5>
+							<h6 className="bookAuthor">{props.productAuthor}</h6>
+						</div>
+						<div className="text-center productPrice">
+							<h5 className="fw-bolder bookPrice">{props.productPrice}€</h5>
+						</div>
 					</div>
-					<div className="text-center productPrice">
-						<h5 className="fw-bolder bookPrice">{props.productPrice}€</h5>
+					<div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+						<div className="text-center"><a className="btn btn-outline-dark mt-auto bookDetails" href="#">{language.productDetails}</a></div>
 					</div>
-				</div>
-				<div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-					<div className="text-center"><a className="btn btn-outline-dark mt-auto bookDetails" href="#">{language.productDetails}</a></div>
 				</div>
 			</div>
-		</div>
 		</div>
 	);
 }
