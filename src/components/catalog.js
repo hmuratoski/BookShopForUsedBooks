811fda00f11df2f75a813ccab259d17a30d9bac8
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-08 10:05:23 +0200

    updated calling Product child component

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -3,26 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Product } from './product';
 import '../css/Catalog.css'
 
-
-
-
-//listaa tuotteet haun/filtterin perusteella käyttäen "Product" komponenttia
-
-const books = [             //from database
-	{ 'id': '1', 'image': require('../images/product.png'), 'price': '50', 'name': 'Kirja1', 'author': 'Kirjoittaja'},
-	{ 'id': '2', 'image': require('../images/product2.png'), 'price': '40', 'name': 'Kirja2', 'author': 'Kirjoittaja'},
-	{ 'id': '3', 'image': require('../images/product3.png'), 'price': '30', 'name': 'Kirja3', 'author': 'Kirjoittaja'},
-	{ 'id': '4', 'image': require('../images/product4.png'), 'price': '20', 'name': 'Kirja4', 'author': 'Kirjoittaja'},
-	{ 'id': '5', 'image': require('../images/product5.png'), 'price': '10', 'name': 'Kirja5', 'author': 'Kirjoittaja'},
-	{ 'id': '6', 'image': require('../images/product4.png'), 'price': '25', 'name': 'Kirja6', 'author': 'Kirjoittaja'},
-	{ 'id': '7', 'image': require('../images/product.png'), 'price': '65', 'name': 'Kirja7', 'author': 'Kirjoittaja'},
-	{ 'id': '8', 'image': require('../images/product2.png'), 'price': '15', 'name': 'Kirja8', 'author': 'Kirjoittaja'},
-	{ 'id': '9', 'image': require('../images/product.png'), 'price': '23', 'name': 'Kirja9', 'author': 'Kirjoittaja'},
-	{ 'id': '10', 'image': require('../images/product3.png'), 'price': '16', 'name': 'Kirja10', 'author': 'Kirjoittaja'},
-	{ 'id': '11', 'image': require('../images/product5.png'), 'price': '17', 'name': 'Kirja11', 'author': 'Kirjoittaja'},
-	{ 'id': '12', 'image': require('../images/product2.png'), 'price': '14', 'name': 'Kirja12', 'author': 'Kirjoittaja' }
+const books = [             //nyt tarvitaan pelkkä kuvatiedoston nimi databasesta, lisätty "product" kansio tuotekuville
+	{ 'bookId': '1','image': 'product.png', 'price': '50', 'bookName': 'Kirja1', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '2', 'image': 'product2.png', 'price': '40', 'bookName': 'Kirja2', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '3', 'image': 'product3.png', 'price': '30', 'bookName': 'Kirja3', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '4', 'image': 'product4.png', 'price': '20', 'bookName': 'Kirja4', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '5', 'image': 'product5.png', 'price': '10', 'bookName': 'Kirja5', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '6', 'image': 'product4.png', 'price': '25', 'bookName': 'Kirja6', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '7', 'image': 'product.png', 'price': '65', 'bookName': 'Kirja7', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '8', 'image': 'product2.png', 'price': '15', 'bookName': 'Kirja8', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '9', 'image': 'product.png', 'price': '23', 'bookName': 'Kirja9', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '10', 'image': 'product3.png', 'price': '16', 'bookName': 'Kirja10', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '11', 'image': 'product5.png', 'price': '17', 'bookName': 'Kirja11', 'author': 'Kirjoittaja', 'year': '2015'},
+	{ 'bookId': '12', 'image': 'product2.png', 'price': '14', 'bookName': 'Kirja12', 'author': 'Kirjoittaja', 'year': '2015' }
 ]
-
 
 export const Catalog = () => {
 	return (
@@ -30,11 +24,12 @@ export const Catalog = () => {
 			<div className="container px-4 px-lg-5">
 				<div className="row gx-4 gx-lg-5 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-5 justify-content-center catalogContainer">
 					{books.map((item) => (
-						<Product    key={item.id} 
+						<Product    key={item.bookId} 
 									productPrice={item.price}
-									productName={item.name} 
-									productImage={item.image}
+									productName={item.bookName} 
+									productImage={require(`../images/product/${item.image}`)}
 									productAuthor={item.author}
+									productYear={item.year}
 						/>
 					))}
 				</div>
