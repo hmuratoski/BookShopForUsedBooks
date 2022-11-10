913febd0ea383c15373c15ae44521c5494ac9897
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-10 17:13:28 +0200

    1

--- src/database/index.php
+++ src/database/index.php
@@ -3,33 +3,27 @@
 	
 	global $json;
 	require_once 'inc/functions.php';
-	$db = openSQLite();         
-	$action = "getBooks"; //kovakoodi purkka, hävitetään myöhemmin, voi vaihtaa jos haluaa testailla
-								//tulee siis requestin mukana
-	switch ($action) {  //tein case switchin, että php tietää mitä yritetään saavuttaa
-	    case "getBooks":
-	        $query = 'select * from BOOKS';
-	        break;
-	    case "getCat":
-			$query = 'select * from CATEGORY';
-	        break;
-		case "getCat1":
-			$query = 'select * from BOOKS where categoryId = 1';
-	        break;
-		case "getCat2":
-			$query = 'select * from BOOKS where categoryId = 2';
-			break;
-		case "getCat3":
-			$query = 'select * from BOOKS where categoryId = 3';
-			break;
-	}
+	$db = openSQLite();
+	if (isset($_GET["action"])) {
+		$action = $_GET["action"];
+		
+		switch ($action) {  //tein case switchin, että php tietää mitä yritetään saavuttaa
+			case "getBooks":
+				$query = 'select * from BOOKS';
+				break;
+			case "getCat":
+				if (isset($_GET["category"]))
+				$query = 'select * from BOOKS where categoryId = ' . $_GET["category"];
+				break;
+		} try {
+			$json = selectAsJson($db, $query);
+			$json = json_encode($json);
+			print $json;
+		} catch (PDOException $pdoex) {
+			returnError($pdoex);
+		}
+	} else {
 	
-	try {
-		$json = selectAsJson($db, $query);
-		$json = json_encode($json);
-		print $json;
-	} catch (PDOException $pdoex) {
-		returnError($pdoex);
 	}
 	
 ?>
