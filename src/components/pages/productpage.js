956b4a80a8a53c6519801a5cc92260b98b9daa93
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-15 18:28:07 +0200

    .

--- src/components/pages/productpage.js
+++ src/components/pages/productpage.js
@@ -18,7 +18,6 @@ export const ProductPage = (props) => {
 			if (response.data.length > 0) {
 				for (var i = 0; i < response.data.length; i++) {
 					details.push(response.data[i]);
-					console.log(response.data[i]);
 				}
 				setBookDetails(details);
 			}
