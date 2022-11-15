fd3af1916dd11c6092c212fed33ed0580ff127e4
Jantte90 <janne.roman@gmail.com>
2022-11-15 09:32:30 -0800

    sfdsf

--- src/components/content.js
+++ src/components/content.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Shop } from './pages/shop';
 import { About } from './pages/about';
-import { Frontpage } from './pages/frontpage';
+import { FrontPage } from './pages/frontpage';
 import { ProductPage } from './pages/productpage';
 import '../css/Header.css';
 
 //Headerin ja footerin välinen sisältö, tänne routingit
 
 export const Content = (props) => {
+
+	{/* <BrowserRouter>
+      <Routes>
+        <Route path="" element={<FrontPage />} />
+		
+		<Route path="/shop" element={<Shop loggedIn={props.loggedIn} />} />
+        
+      </Routes>
+</BrowserRouter> */ }
+
+
 	switch (props.page[0]) {                //yksittäisten sivujen testaukseen ennen routingia
 		case "Home":
 			return (
-				<Frontpage />
+				<FrontPage />
 			);
 			
 		case "ProductPage":
