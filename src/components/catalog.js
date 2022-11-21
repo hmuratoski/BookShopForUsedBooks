684c92cfe6b639ef345e544016cf67511b527d94
n2saju00 <111992893+n2saju00@users.noreply.github.com>
2022-11-21 21:03:51 +0200

    puuttuvan tuotekuvan käsittely

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -11,20 +11,30 @@ export const Catalog = (props) => {     //kirjat tulee nyt propsina <-- shop.js
 			<section>
 				<div className="container px-4 px-lg-5">
 					<div className="row gx-4 gx-lg-5 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-5 justify-content-center catalogContainer">
-						{props.booksToDisplay.map((item) => (
-							<Product
-								loggedIn={props.loggedIn}
-								key={item.bookId}
-								productId={item.bookId}
-								productPrice={item.price}
-								productName={item.bookName}
-								productImage={require(`../images/product/${item.bookId}.png`)}
-								productAuthor={item.author}
-								productYear={item.year}
-								productCondition={item.condition}
-								addToCart={props.addToCart}
-							/>
-						))}
+						{props.booksToDisplay.map((item) => {
+							let src;
+						    try {
+						        src = require(`../images/product/${item.bookId}.png`);
+						    }
+						    catch {
+						        let randomNumber = Math.floor(Math.random() * (5 - 1 + 1) + 1)
+						        src = require(`../images/product/product${randomNumber}.png`);          //jos tuotekuva puuttuu, laitetaan random placeholderi tilalle
+						    }
+							return (
+								<Product
+									loggedIn={props.loggedIn}
+									key={item.bookId}
+									productId={item.bookId}
+									productPrice={item.price}
+									productName={item.bookName}
+									productImage={src}
+									productAuthor={item.author}
+									productYear={item.year}
+									productCondition={item.condition}
+									addToCart={props.addToCart}
+								/>
+							)
+						})}
 					</div>
 				</div>
 			</section>
