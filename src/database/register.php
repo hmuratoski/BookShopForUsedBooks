fe2052b9bac285a5533711bb1626ad84314caf13
hmuratoski <98337481+hmuratoski@users.noreply.github.com>
2022-11-29 19:32:03 +0200

    alustava registerointi  back endille;

--- src/database/register.php
+++ src/database/register.php
@@ -1,2 +1,22 @@
 <?php
 
+require('./headers.php');
+session_start();
+
+$body = file_get_contents("php://input");
+$user = json_decode($body);
+
+if(!isset($customerId->username) || !isset($username->password)){
+    http_response_code(400);
+    echo "User not defined. Give valid username and password";
+    return;
+}
+
+
+registerUser($customerId->username, $username->password);
+
+$_SESSION['username'] = $user->uname;
+
+http_response_code(200);
+echo "User ".$customer->username." registered";
+
