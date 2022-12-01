a66642a36860e861f1019cd9aee409ab5a42a819
n2saju00 <n2saju00@students.oamk.fi>
2022-12-01 02:41:20 +0200

    duplicate user check

--- src/database/register.php
+++ src/database/register.php
@@ -19,7 +19,7 @@ if($err != 0) {
     echo "Missing information";
     return;
 } else {
-    $checkUserExist = "select * from USER where username = '" . $_POST["username"]."'";
+    $checkUserExist = "select * from USER where (username = '" . $_POST["username"]."' or email = '" . $_POST["email"] . "')";
     $checkUserExist = selectAsJson($db, $checkUserExist);
     if (count($checkUserExist) != 0) {
         http_response_code(409);
