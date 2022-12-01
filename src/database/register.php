678c81b0be431e5c706f7887ec713997ae7556fa
n2saju00 <n2saju00@students.oamk.fi>
2022-12-01 02:31:51 +0200

    register.php

--- src/database/register.php
+++ src/database/register.php
@@ -5,40 +5,49 @@ require_once('./inc/functions.php');
 
 $db = openSQLite();
 
-if( !isset($_POST["username"]) || 
-    !isset($_POST["password"]) || 
-    !isset($_POST["fname"]) || 
-    !isset($_POST["lname"]) || 
-    !isset($_POST["address"]) || 
-    !isset($_POST["postalcode"]) || 
-    !isset($_POST["city"]) || 
-    !isset($_POST["email"]) || 
-    !isset($_POST["phone"])
-) {
+$requiredInfo = array('username','password','fname','lname','address','postalcode','city','email','phone');
+$err = 0;
+
+foreach ($requiredInfo as $key) {
+    if(!isset($_POST[$key])) {
+        $err++;
+    }
+}
+
+if($err != 0) {
     http_response_code(400);
     echo "Missing information";
     return;
 } else {
-    $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
-    $sql = "INSERT INTO USER(username,password,fname,lname,address,postalcode,city,email,phone,city) VALUES('";
-    $sql .= $_POST["username"] . "','";
-    $sql .= $hash . "','";
-    $sql .= $_POST["fname"] . "','";
-    $sql .= $_POST["lname"] . "','";
-    $sql .= $_POST["address"] . "','";
-    $sql .= $_POST["postalcode"] . "','";
-    $sql .= $_POST["city"] . "','";
-    $sql .= $_POST["email"] . "','";
-    $sql .= $_POST["phone"] . "','";
-    $sql .= $_POST["city"] . "')";
-    try {
-        executeInsert($db, $sql);
-        echo "Success";
-        $_SESSION['username'] = $_POST["username"];
-        http_response_code(200);
-    } catch (Exception $e) {
-        echo "Failed";
-        print_r($e);
+    $checkUserExist = "select * from USER where username = '" . $_POST["username"]."'";
+    $checkUserExist = selectAsJson($db, $checkUserExist);
+    if (count($checkUserExist) != 0) {
+        http_response_code(409);
+        echo "User already exists";
+        return;
+    } else {
+
+        $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
+        $sql = "INSERT INTO USER(username,password,fname,lname,address,postalcode,city,email,phone,city) VALUES('";
+        $sql .= $_POST["username"] . "','";
+        $sql .= $hash . "','";
+        $sql .= $_POST["fname"] . "','";
+        $sql .= $_POST["lname"] . "','";
+        $sql .= $_POST["address"] . "','";
+        $sql .= $_POST["postalcode"] . "','";
+        $sql .= $_POST["city"] . "','";
+        $sql .= $_POST["email"] . "','";
+        $sql .= $_POST["phone"] . "','";
+        $sql .= $_POST["city"] . "')";
+        try {
+            executeInsert($db, $sql);
+            echo "Success";
+            $_SESSION['username'] = $_POST["username"];
+            http_response_code(200);
+        } catch (Exception $e) {
+            echo "Failed";
+            print_r($e);
+        }
     }
 }
 
