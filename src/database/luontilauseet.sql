8b9a891734a2d6a50c9ca5cf5ac9475207f688c5
n2saju00 <n2saju00@students.oamk.fi>
2022-12-05 12:26:20 +0200

    SQL luontilauseet korjattu

--- src/database/luontilauseet.sql
+++ src/database/luontilauseet.sql
@@ -1,30 +1,40 @@
-CREATE TABLE ORDER (
-	orderId varchar PRIMARY KEY,
+DROP TABLE BOOK;
+DROP TABLE CATEGORY;
+DROP TABLE ORDERS;
+DROP TABLE ORDER_ITEMS;
+DROP TABLE USER;
+DROP TABLE ADMIN;
+
+
+CREATE TABLE ORDERS (
+	orderId integer PRIMARY KEY,
 	customerId varchar,
 	orderStatus varchar,
-	orderDate varchar,
-	shippedDate varchar
+	orderDate datetime,
+	shippedDate datetime
 );
 
 CREATE TABLE BOOK (
 	bookId varchar PRIMARY KEY,
-	categoryId varchar,
+	categoryId integer,
 	bookName varchar,
-	price varchar,
+	price float,
 	author varchar,
-	description varchar
+	description varchar,
+	year smallint,
+	condition varchar
 );
 
 CREATE TABLE CATEGORY (
-	categoryId varchar PRIMARY KEY,
+	categoryId integer PRIMARY KEY,
 	categoryName varchar
 );
 
 CREATE TABLE ORDER_ITEMS (
-	orderId varchar,
-	bookId varchar,
-	quantity binary,
-	price varchar
+	orderId integer,
+	bookId integer,
+	quantity integer,
+	price float
 );
 
 CREATE TABLE USER (
@@ -37,6 +47,14 @@ CREATE TABLE USER (
 	email varchar,
 	address varchar,
 	city varchar,
-	stateprovince varchar,
+	stateProvince varchar,
 	postalcode varchar
 );
+
+CREATE TABLE ADMIN (
+	username varchar,
+	level integer
+);
+
+INSERT INTO BOOK(bookId,categoryId,bookName,price,author,description,year,condition) VALUES('1','1','Matematiikan alkeet','25','Mikko Matikka','Ala-asteen lyhyt matematiikka','2011','3'),('2','1','Matematiikan perusteet','30','Mikko Matikka','Ylä-aste matematiikka','2012','2'),('3','1','Lukion matematiikka','15','Mikko Matikka','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac tortor porttitor, sodales magna id, iaculis augue. Morbi consequat mollis dui ut aliquet. Cras efficitur nisi quis suscipit elementum. Morbi tristique bibendum urna et lacinia. Nullam commodo et ligula et vestibulum. Phasellus varius dolor vel ex venenatis, eu ultricies neque ullamcorper. Aenean facilisis fringilla justo eget vestibulum. In vehicula nisi vitae felis elementum semper. Nulla non est a purus vestibulum ultricies nec nec neque. Duis ultrices gravida lacinia. Praesent a aliquam ex, non dignissim velit. Pellentesque justo arcu, porta sit amet ullamcorper at, suscipit eu nibh. Quisque vitae imperdiet odio.\\n\\nSed lobortis a felis in cursus. Donec egestas dui est, eget maximus arcu vehicula in. Nulla facilisi. Donec rhoncus odio augue, ut auctor ligula dignissim commodo. Cras aliquet sed dolor non fringilla. Praesent faucibus eros ex, nec porttitor velit semper id. Etiam quis aliquet ex. Vivamus maximus mi quis eros lacinia vestibulum et ac turpis. Nunc dictum nunc at urna aliquam, et viverra metus facilisis.\\n\\nMauris eget mauris tortor. Nunc fringilla metus sed consectetur cursus. Sed sagittis dolor egestas semper ornare. Donec posuere risus at lorem tristique placerat. In eu elementum purus, vel porta metus. Phasellus fringilla sapien elit, vitae egestas justo efficitur eget. Mauris vel eros rutrum, rutrum neque vel, porta nisl.','2015','1'),('4','2','Suomen kielen alkeet','60','Arttu Mokoma','Opi suomea, viikossa!','2018','3'),('5','3','Kemia 1','55','Niko Pyhtilä','Kemian alkeet','1995','2'),('6','4','Maantiede 1','75','Pekka Rinne','Maantiede alakoululaisille','1952','1'),('7','5','Suomen historia','87','Moona Meikäläinen','Suomen historia','2011','3');
+INSERT INTO CATEGORY(categoryId,categoryName) VALUES('1','Matematiikka'),('2','Kielet'),('3','Kemia'),('4','Maantiede'),('5','Historia');
