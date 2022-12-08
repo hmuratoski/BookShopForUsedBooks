1162af109105328c543a6ec36709ccd207dde54d
Jantte90 <janne.roman@gmail.com>
2022-12-07 18:17:51 -0800

    3fdgdfg

--- src/database/adminaddbook.php
+++ src/database/adminaddbook.php
@@ -3,8 +3,8 @@ require('./inc/headers.php');
 require_once('./inc/functions.php');
 
 session_start();
-if (isset($_SESSION["username"])) {
 
+if (isset($_SESSION["username"])) {
 
 $db = openSQLite();
   
