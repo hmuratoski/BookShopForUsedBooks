de8d19f27db8fd4acbc23f175ccb6e51c2898969
n2saju00 <n2saju00@students.oamk.fi>
2022-12-12 11:09:24 +0200

    .

--- src/database/order.php
+++ src/database/order.php
@@ -1,19 +1,23 @@
 <?php
+require('./inc/headers.php');
+require_once('inc/functions.php');
 
 $db = openSQLite();
 
 if (!isset($_GET["action"])) {
     echo "no action";
     http_response_code(400);
+    return;
 } else if (!isset($_SESSION["username"])) {
     echo "not logged in";
     http_response_code(400);
+    return;
 }
 
-    $action = $_GET["action"];
+$action = $_GET["action"];
     
-    switch ($action) {
-        case "makeOrder":
-            
-        break;
-    }
+switch ($action) {
+    case "makeOrder":
+
+    break;
+}
