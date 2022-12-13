b6dadff3679b4a7eb5a5ee9e85ce1519a99f576c
hmuratoski <98337481+hmuratoski@users.noreply.github.com>
2022-12-14 00:10:04 +0200

    Etusivu

--- src/components/pages/frontpage.js
+++ src/components/pages/frontpage.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../css/Frontpage.css';
 
 //Kaupan etusivu. tietoja sitä tätä tuota, ehkä jotakin kuvaa
 
 export const FrontPage = () => {
 	return (
-		<div>
-			<h1>ETUSIVU (frontpage.js)</h1>
-		</div>
+<div className="container-fluid">
+  <div className="container-xl">
+    
+  <h1 className='otsikko'>Tervetuloa Kirijoja verkkokauppaan!</h1>
+  </div>
+  <p className='yleisteksti'>Täältä löydät erilliaisia käytettyjä oppikirjoja.
+     Pääset hakemaan kirjat hakukenttän avulla, kategorian sekä kirjan nimeella. Ja tiettysti, HALAVALLA!</p>
+     <p className='tracking-out-expand-fwd '>Kirijoja kaikille, HALAVALLA!</p>
+
+</div>
+
+
+
 	);
 }
